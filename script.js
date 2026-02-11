85d957cca8436a3ce9a3df4d493f21eb7ae9a548
yavuzkara620 <ahmetyavuz1718@gmail.com>
2026-02-11 22:22:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ window.addEventListener('DOMContentLoaded', () => {
     validateConfig();
 
     // Set texts from config
-    document.getElementById('valentineTitle').textContent = `${config.valentineName}, my love...`;
+    document.getElementById('valentineTitle').textContent = `${config.valentineName}, aşkımm...`;
     
     // Set first question texts
     document.getElementById('question1Text').textContent = config.questions.first.text;
